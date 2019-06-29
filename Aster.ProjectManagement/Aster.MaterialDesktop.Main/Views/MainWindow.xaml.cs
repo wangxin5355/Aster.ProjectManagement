6239c72ff89dc259bb69d6372d409b8dae89f96d
wangxin <wangxin@hopexfintech.com>
2019-06-29 16:59:55 +0800

    1，窗体样式

--- Aster.ProjectManagement/Aster.MaterialDesktop.Main/Views/MainWindow.xaml.cs
+++ Aster.ProjectManagement/Aster.MaterialDesktop.Main/Views/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ namespace Aster.MaterialDesktop.Main.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
